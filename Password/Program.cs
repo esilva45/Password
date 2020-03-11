99dc0b3b0679be40e48619ac8fe2813ceeedb6b2
Elso Silva <esilva45@hotmail.com>
2020-03-11 10:09:24 -0300

    Adicionar arquivos de projeto.

---
+++ Password/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Password {
+    class Program {
+        static void Main(string[] args) {
+
+            try {
+                string pass = Convert.ToString(args[0]);
+
+                if (pass != "") {
+                    Console.WriteLine("Senha:" + pass + " Criptografia: " + SecurePassword.Encrypt(pass));
+                } else {
+                    Console.WriteLine("Informe a senha");
+                }
+            } catch {
+                Console.WriteLine("Informe a senha");
+            }
+        }
+    }
+}
